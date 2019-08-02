994ba0eaef2ab64a03d10ad404172b8190ba1630
Buck Tower <buck@bucktower.net>
2019-08-02 02:31:35 +0000

    More updates again

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,29 @@
-$(window).load(function() {
-  $("body").removeClass("preload");
-});
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+var onresize = function() {
+   //your code here
+   //this is just an example
+   width = document.body.clientWidth;
+   height = document.body.clientHeight;
+   
+  var x = document.getElementById("mobileNav");
+  var y = document.getElementById("nvbr");
+   
+   if (width <= 770) {
+     x.style.display = "block";
+     y.style.display = "none";
+     
+   } else {
+     x.style.display = "none";
+     y.style.display = "flex";
+     console.log(width);
+   }
+}
+window.addEventListener("resize", onresize);
