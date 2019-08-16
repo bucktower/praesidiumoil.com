5c6934e0ef4472a5963e3a7308b1ae7d6914b432
Buck Tower <buck@bucktower.net>
2019-08-16 04:20:01 +0000

    mobile nav and new pic

--- scripts/main.js
+++ scripts/main.js
@@ -19,8 +19,8 @@ var onintro = function() {
 
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginRight = "250px";
+  document.getElementById("mySidenav").style.width = "500px";
+  document.getElementById("main").style.marginRight = "500px";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
