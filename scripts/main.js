f9c377f33cc952913ea98538de8efec4dd63d6b5
Buck Tower <buck@bucktower.net>
2019-10-02 07:03:52 +0000

    More changes

--- scripts/main.js
+++ scripts/main.js
@@ -20,14 +20,14 @@ var onintro = function() {
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
   document.getElementById("mySidenav").style.width = "500px";
-  document.getElementById("main").style.marginRight = "500px";
+  // document.getElementById("main").style.marginRight = "500px";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginRight = "0";
+  // document.getElementById("main").style.marginRight = "0";
   document.body.style.backgroundColor = "white";
 }
 
